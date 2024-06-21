5aa8639ab637317af2b0aa771ea615eaa401f4a1
jonroy47 <jonroy47@kingshigh.school.nz>
2024-06-21 13:24:22 +1200

    zrgs
    srtgh

---
+++ Car.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var wheel_base = 70
+var steering_angle = 15
+
+var velocity = Vector2.ZERO
+var steer_direction
+
+func _physics_process(delta):
+	get_input()
+	calculate_steering(delta)
+	velocity - move_and_slide()
+	
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("steer_right"):
+		turn += 1
+	if Input.is_action_pressed("steer_left"):
+		turn -= 1
+		steer_direction = turn * steering_angle
+		velocity = Vector2.ZERO
+		if Input.is_action_pressed("accelerate"):
+			velocity = transform.x * 500
+			
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base/2.0
+	var front_wheel = position + transform.x * wheel_base/2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction * delta)
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	velocity = 
