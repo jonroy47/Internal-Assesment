4ee32ec07de6fd448c5c5e45078ad3c7f2adc8fd
jonroy47 <jonroy47@kingshigh.school.nz>
2024-06-24 14:59:46 +1200

    dfth
    drth

--- Car.gd
+++ Car.gd
@@ -9,7 +9,6 @@ var steer_direction
 func _physics_process(delta):
 	get_input()
 	calculate_steering(delta)
-	direction - move_and_slide()
 	
 func get_input():
 	var turn = 0
@@ -17,16 +16,16 @@ func get_input():
 		turn += 1
 	if Input.is_action_pressed("steer_left"):
 		turn -= 1
-		steer_direction = turn * steering_angle
-		velocity = Vector2.ZERO
-		if Input.is_action_pressed("accelerate"):
-			velocity = transform.x * 500
-			
+	steer_direction = turn * steering_angle
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("forward"):
+		velocity = transform.x * 500
+		
 func calculate_steering(delta):
 	var rear_wheel = position - transform.x * wheel_base/2.0
 	var front_wheel = position + transform.x * wheel_base/2.0
 	rear_wheel += velocity * delta
-	front_wheel += velocity.rotated(steer_direction * delta)
+	front_wheel += velocity.rotated(steer_direction) * delta
 	var new_heading = (front_wheel - rear_wheel).normalized()
 	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()
