413fb1b453e1a47562aefcaf963e58f60d9c8950
jonroy47 <jonroy47@kingshigh.school.nz>
2024-06-27 10:59:00 +1200

    final works...
    plz no crash

--- Car.gd
+++ Car.gd
@@ -3,30 +3,30 @@ extends CharacterBody2D
 var wheel_base = 70
 var steering_angle = 15
 
-var direction = Vector2.ZERO
+
 var steer_direction
 
 func _physics_process(delta):
 	get_input()
 	calculate_steering(delta)
-	velocity = move_and_slide(velocity)
+	move_and_slide()
 	
 func get_input():
 	var turn = 0
-	if Input.is_action_pressed("steer_right"):
+	if Input.is_action_pressed('r'):
 		turn += 1
-	if Input.is_action_pressed("steer_left"):
+	if Input.is_action_pressed('l'):
 		turn -= 1
 	steer_direction = turn * steering_angle
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("forward"):
-		velocity = transform.x * 500
+	if Input.is_action_pressed('u'):
+		velocity = transform.x * 100
 		
 func calculate_steering(delta):
-	var rear_wheel = position - transform.x * wheel_base/2.0
-	var front_wheel = position + transform.x * wheel_base/2.0
+	var rear_wheel = position - transform.x * wheel_base/7.5
+	var front_wheel = position + transform.x * wheel_base/7.5
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
-	var new_heading = (front_wheel - rear_wheel).normalized()
+	var new_heading = (front_wheel - rear_wheel)
 	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()
