6ed8445d2c17feeedf780cb97f7788dce9de7d43
jonroy47 <jonroy47@kingshigh.school.nz>
2024-07-03 15:00:12 +1200

    Works good 1.1
    I made a shift key powerslide. Added tilemap also

--- Car.gd
+++ Car.gd
@@ -1,19 +1,17 @@
 extends CharacterBody2D
-
-
-var wheel_base = 70
-var steering_angle = 15
-var engine_power = 8000
+var acceleration = Vector2.ZERO
+var engine_power = 500
 var friction = -0.9
 var drag = -0.001
-var braking = -450
+var braking = -350
 var max_speed_reversed = 250
-var slip_speed = 400
+var slip_speed = 300
 var traction_fast = 0.1
-var traction_slow = 0.7
-
-var acceleration = Vector2.ZERO
-
+var traction_slow = 0.8
+# When the Car fets up to speed, i.e. traction_fast, the car will lose traction
+# so it looks like it is drifting or sliding.
+var wheel_base = 70
+var steering_angle = 20
 var steer_direction 
 
 func _physics_process(delta):
@@ -26,16 +24,24 @@ func _physics_process(delta):
 
 func get_input():
 	var turn = 0
-	if Input.is_action_pressed("d"):
+	if Input.is_action_pressed("right"):
 		turn += 1
-	if Input.is_action_pressed("a"):
+	if Input.is_action_pressed("left"):
 		turn -= 1
 	steer_direction = turn * deg_to_rad(steering_angle)
-	
-	if Input.is_action_pressed("w"):
+	if Input.is_action_pressed("forward"):
 		acceleration = transform.x * engine_power
-	if Input.is_action_pressed("s"):
+	if Input.is_action_pressed("backward"):
 		acceleration = transform.x * braking
+	if Input.is_action_pressed("powerslide"):
+		traction_fast = 0.001
+		traction_slow = 0.1
+		steering_angle = 40
+	else:
+		traction_fast = 0.1
+		traction_slow = 0.8
+		steering_angle = 20
+# This is Powerslide so you will drift  if you press down 'SHIFT'
 
 func calculate_steering(delta):
 	var rear_wheel = position - transform.x * wheel_base/2.0
