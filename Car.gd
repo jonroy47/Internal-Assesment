d6b3ea2699f6e97b66da6c4fbec1d8b4a9a05ab6
jonroy47 <jonroy47@kingshigh.school.nz>
2024-07-24 13:55:10 +1200

    Added Enemy Sprite
    About to add exp etc and death effect

--- Car.gd
+++ Car.gd
@@ -5,13 +5,13 @@ var friction = -0.9
 var drag = -0.001
 var braking = -450
 var max_speed_reversed = 100
-var slip_speed = 250
-var traction_fast = 0.1
-var traction_slow = 0.8
+var slip_speed = 200
+var traction_fast = 0.09
+var traction_slow = 0.9
 # When the Car fets up to speed, i.e. traction_fast, the car will lose traction
 # so it looks like it is drifting or sliding.
 var wheel_base = 70
-var steering_angle = 30
+var steering_angle = 40
 var steer_direction 
 
 func _physics_process(delta):
@@ -36,11 +36,11 @@ func get_input():
 	if Input.is_action_pressed("powerslide"):
 		traction_fast = 0.001
 		traction_slow = 0.1
-		steering_angle = 60
+		steering_angle = 70
 	else:
 		traction_fast = 0.1
 		traction_slow = 0.8
-		steering_angle = 30
+		steering_angle = 40
 # This is Powerslide so you will drift  if you press down 'SHIFT'
 
 func calculate_steering(delta):
@@ -63,3 +63,5 @@ func apply_friction():
 	var friction_force = velocity * friction
 	var drag_force = velocity * velocity.length() * drag
 	acceleration += drag_force + friction_force
+	
+
---
+++ other_loot_box.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func collected():
+	var vanish = preload("res://LootBox.tscn")
+	var new_vanish_object = vanish.instantiate()
+	get_tree().current_scene.add_child(new_vanish_object)
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Car"):
+		collected()
+		queue_free()
+
